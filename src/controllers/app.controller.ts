c7509f1fa51ebc5a9176ba046948bdc42107f1a9
emanuel palacio <emanuelpalacio.dev@gmail.com>
2026-02-11 22:19:57 -0300

    fix: mark root endpoint as public for health checks

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../decorators/common/auth';
 
 @Controller()
 export class AppController {
   @Get()
+  @Public()
   getHello(): string {
     return 'Urgara Jornales API 🚀';
   }
